9392099b031ffe2fec2df8707d15a69e18a4e036
Alexia Paré <alexiakawaai@gmail.com>
2026-03-05 15:30:54 -0300

    Update script.js
    Added functionality to prevent duplicate numbers and handle deletions.

--- js/script.js
+++ js/script.js
@@ -12,23 +12,27 @@ const menor = document.getElementById("menor")
 const btnFinalizar = document.getElementById("btn-finalizar")
 const btnReiniciar = document.getElementById("btn-reiniciar")
 
-
 var listaNumeros = Array()
 
 btnFinalizar.style.cursor = "not-allowed"
 btnFinalizar.setAttribute("disabled", null)
 
 numero.addEventListener("keypress", (elem)=>{
-   if (console.log(elem.key = "Enter")) {
-    btnAdd.click()
-   }
-   console.log.key()
+    if( elem.key == "Enter" ) {
+        btnAdd.click()
+    }
+    
+    //console.log(elem.key)
 })
 
 btnAdd.addEventListener("click", ()=>{
     if( numero.value == "" ) {
         alert("Insira um número!")
+    } else if( testaNumero() != undefined ) {
+        alert("Número " + numero.value + " já inserido.")
+        limpaCampo()
     } else {
+        //console.log( testaNumero() )
         listaNumeros.push( Number(numero.value) )
         
         txtContar.innerHTML = "Restam " +
@@ -43,9 +47,10 @@ btnAdd.addEventListener("click", ()=>{
         mudaBotao()
         limpaCampo()
 
-        console.log(listaNumeros)
+        //console.log(listaNumeros)
     }
 })
+
 
 btnFinalizar.addEventListener("click", ()=>{
     primeiro.innerHTML = listaNumeros[0]
@@ -55,22 +60,59 @@ btnFinalizar.addEventListener("click", ()=>{
     menor.innerHTML = listaNumeros.reduce((a, b)=>Math.min(a, b))
 })
 
-btnReiniciar.addEventListener("click", ()=> {
+
+btnReiniciar.addEventListener("click", ()=>{
     window.location.reload()
 })
 
-function mudaBotao(){
-    if(listaNumeros.length == 10){
+function mudaBotao() {
+    if( listaNumeros.length == 10 ) {
         numero.setAttribute("disabled", null)
         numero.style.cursor = "not-allowed"
         btnAdd.setAttribute("disabled", null)
         btnAdd.style.cursor = "not-allowed"
-        btnFinalizar.removeAttribute("disebled")
+        btnFinalizar.removeAttribute("disabled")
         btnFinalizar.style.cursor = "pointer"
     }
 }
 
-function limpaCampo(){
+function limpaCampo() {
     numero.value = ""
     numero.focus()
 }
+
+function testaNumero() {
+    return listaNumeros.find((v)=> numero.value == v)
+}
+
+btnApagar.addEventListener("click", ()=>{
+    let selecionados = []
+
+    for( let i=0; i<inseridos.options.length; i++ ) {
+        if( inseridos.options[i].selected ) {
+            selecionados.push(inseridos.options[i].value)
+        }
+    }
+    
+    //console.log( selecionados )
+
+    selecionados.forEach((v)=>{
+        listaNumeros = listaNumeros.filter((e)=> e != v)
+        document.getElementById(v+"-id").remove()
+    })
+
+    if( listaNumeros.length < 10 ) {
+        numero.removeAttribute("disabled")
+        numero.style.cursor = "pointer"
+        btnAdd.removeAttribute("disabled")
+        btnAdd.style.cursor = "pointer"
+        btnFinalizar.setAttribute("disabled", null)
+        btnFinalizar.style.cursor = "not-allowed"
+    }
+
+    txtContar.innerHTML = "Restam " +
+                     (10 - listaNumeros.length) +
+                     " números"
+                     
+    console.log(listaNumeros)
+})
